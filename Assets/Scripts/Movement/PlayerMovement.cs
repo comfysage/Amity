2581e17ca6246f92170364678443beb6bbf5fe39
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-25 17:05:26 +0200

    Moved to JumpActions Class

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -8,14 +8,18 @@ public class PlayerMovement : MonoBehaviour
   [SerializeField]
   Rigidbody2D rb;
 
-  public void Jump(InputAction.CallbackContext context)
+  public void JumpAction(InputAction.CallbackContext context)
   {
     rb.AddForce(Vector2.up * 1.8f, ForceMode2D.Impulse);
   }
 
-  void Start()
+  void Awake()
   {
     rb = GetComponent<Rigidbody2D>();
+
+    PlayerInputActions playerInputActions = new PlayerInputActions();
+    playerInputActions.Player.Enable();
+    playerInputActions.Player.Jump.performed += JumpAction;
   }
 
   void Update()
