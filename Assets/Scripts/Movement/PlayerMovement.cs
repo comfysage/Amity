d66f8c21e301602bea7e59ef4201b511a0b18408
DinoVincent <106249505+DinoVincent@users.noreply.github.com>
2022-05-25 22:15:00 +0200

    Cleanup code Bowen

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -10,12 +10,22 @@ public class PlayerMovement : MonoBehaviour
   Rigidbody2D rb;
   BoxCollider2D bc;
   PlayerInputActions playerInputActions;
-
+  [SerializeField]
+  [Header("Movement")]
   float movementSpeed = 3.2f;
+  [SerializeField]
   bool speedCap = true;
-
+  [Space(2f)]
+  [Header("Jumping")]
+  [SerializeField]
   float jumpForce = 5;
+  [SerializeField]
   int jumpCount = 0;
+
+  [Space(2f)]
+  [Header("Debugging")]
+  [SerializeField]
+  bool drawGroundcheckRaycast;
 
   void JumpAction(InputAction.CallbackContext context)
   {
@@ -42,19 +52,14 @@ public class PlayerMovement : MonoBehaviour
   void FixedUpdate()
   {
     Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
-    if (inputVector.x > 0)
-    {
-      transform.localScale = new Vector3(2, 2, 2);
-    }
-    if (inputVector.x < 0)
-    {
-      transform.localScale = new Vector3(-2, 2, 2);
-    }
+    if (inputVector.x > 0) transform.localScale = new Vector3(2, 2, 2);
+    
+    if (inputVector.x < 0) transform.localScale = new Vector3(-2, 2, 2);
+
     rb.AddForce(new Vector2(inputVector.x, 0) * movementSpeed, ForceMode2D.Impulse);
-    if (speedCap)
-    {
-      CapVelocity();
-    }
+
+    if (speedCap) CapVelocity();
+   
 
     if (!isGrounded() && jumpCount == 0) jumpCount = 1;
     if (isGrounded()) jumpCount = 0;
@@ -66,15 +71,12 @@ public class PlayerMovement : MonoBehaviour
     RaycastHit2D raycastHit = Physics2D.Raycast(bc.bounds.center, Vector2.down, bc.bounds.extents.y + extraHeight, environmentMask);
 
     Color rayColor;
+    if(drawGroundcheckRaycast){ //Debug raycast
     if (raycastHit.collider != null)
-    {
-      rayColor = Color.green;
-    }
-    else
-    {
-      rayColor = Color.red;
-    }
+    rayColor = Color.green;
+    else rayColor = Color.red;
     Debug.DrawRay(bc.bounds.center, Vector2.down * (bc.bounds.extents.y + extraHeight), rayColor);
+    }
 
     return raycastHit.collider != null;
   }
