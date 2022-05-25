ea13e78a38aaaca4fd88dfbcafa75eb5969b34b9
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-25 17:40:05 +0200

    Added Movement

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,8 +7,12 @@ public class PlayerMovement : MonoBehaviour
 {
   [SerializeField]
   Rigidbody2D rb;
+  PlayerInputActions playerInputActions;
 
-  public void JumpAction(InputAction.CallbackContext context)
+  float movementSpeed = 6f;
+  bool speedCap = true;
+
+  void JumpAction(InputAction.CallbackContext context)
   {
     rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
   }
@@ -17,13 +21,25 @@ public class PlayerMovement : MonoBehaviour
   {
     rb = GetComponent<Rigidbody2D>();
 
-    PlayerInputActions playerInputActions = new PlayerInputActions();
+    playerInputActions = new PlayerInputActions();
     playerInputActions.Player.Enable();
     playerInputActions.Player.Jump.performed += JumpAction;
   }
 
-  void Update()
+  void FixedUpdate()
   {
+    Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+    rb.AddForce(new Vector2(inputVector.x, 0) * movementSpeed, ForceMode2D.Impulse);
+    if (speedCap)
+    {
+      CapVelocity();
+    }
+  }
 
+  void CapVelocity()
+  {
+    float cappedXVelocity = Mathf.Min(Mathf.Abs(rb.velocity.x), movementSpeed) * Mathf.Sign(rb.velocity.x);
+
+    rb.velocity = new Vector2(cappedXVelocity, rb.velocity.y);
   }
 }
