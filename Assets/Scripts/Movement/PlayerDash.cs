bf6f845f3563796c2499126d9b08b347e1093c91
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-26 16:52:00 +0200

    Added DashAction

--- Assets/Scripts/Movement/PlayerDash.cs
+++ Assets/Scripts/Movement/PlayerDash.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerDash : MonoBehaviour
 {
-  public PlayerMovement playerMovement;
+  public PlayerMovement pm;
+  private PlayerInputActions playerInputActions;
+  private Rigidbody2D rb;
+
+  float dashSpeed = 1.2f;
+
+  void DashAction(InputAction.CallbackContext context)
+  {
+    rb.velocity = new Vector2(rb.velocity.x + (transform.localScale.x * dashSpeed), 0);
+  }
+
+  void Awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+  void Start()
+  {
+    playerInputActions = pm.playerInputActions;
+    playerInputActions.Player.Dash.performed += DashAction;
+  }
 }
