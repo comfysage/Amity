d2745a6c289de801ff99d97d469a2e1f284c2b3f
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-27 14:52:43 +0200

    Cleaned up code

--- Assets/Scripts/Movement/PlayerDash.cs
+++ Assets/Scripts/Movement/PlayerDash.cs
@@ -29,14 +29,16 @@ public class PlayerDash : MonoBehaviour
     _velocity = rb.velocity;
     rb.gravityScale = 0;
     _dashTime = dashTime;
+
     rb.velocity = new Vector2(direction * dashSpeed, 0);
-    // while dashing update position
+
+    // wait till dash is over
     while (_dashTime > 0)
     {
-      // local with position value so moving is blocked
       _dashTime -= Time.deltaTime;
       yield return null;
     }
+    // reset gravity and x velocity
     rb.gravityScale = _gravity;
     rb.velocity = new Vector2(_velocity.x, 0);
   }
