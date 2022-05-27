d99f835d070185a0773e5f54513e5ec20fdca539
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-27 14:38:17 +0200

    Working dash

--- Assets/Scripts/Movement/PlayerDash.cs
+++ Assets/Scripts/Movement/PlayerDash.cs
@@ -9,16 +9,40 @@ public class PlayerDash : MonoBehaviour
   private PlayerInputActions playerInputActions;
   private Rigidbody2D rb;
 
-  float dashSpeed = 1.2f;
+  Vector2 _velocity;
+  float _gravity;
+
+  [SerializeField]
+  float dashSpeed = 12f;
+  [SerializeField]
+  float dashTime = 1;
+  float _dashTime = 1;
 
   void DashAction(InputAction.CallbackContext context)
   {
-    rb.velocity = new Vector2(rb.velocity.x + (transform.localScale.x * dashSpeed), 0);
+    StartCoroutine(Dash());
+  }
+
+  IEnumerator Dash()
+  {
+    float direction = Mathf.Sign(transform.localScale.x);
+    _velocity = rb.velocity;
+    rb.gravityScale = 0;
+    _dashTime = dashTime;
+    while (_dashTime > 0)
+    {
+      transform.position += new Vector3(direction * dashSpeed * Time.deltaTime, 0, 0);
+      _dashTime -= Time.deltaTime;
+      yield return null;
+    }
+    rb.gravityScale = _gravity;
+    rb.velocity = new Vector2(_velocity.x, 0);
   }
 
   void Awake()
   {
     rb = GetComponent<Rigidbody2D>();
+    _gravity = rb.gravityScale;
   }
   void Start()
   {
