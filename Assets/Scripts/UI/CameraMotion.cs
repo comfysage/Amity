4067b12175d2ad5b9815ae23a7ac2637c0b5100f
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-06-02 16:52:12 +0200

    Added Camera Movement offset to CameraMotion

--- Assets/Scripts/UI/CameraMotion.cs
+++ Assets/Scripts/UI/CameraMotion.cs
@@ -7,10 +7,21 @@ public class CameraMotion : MonoBehaviour
   [SerializeField]
   public Transform target;
   Vector3 offset = new Vector3(0, 0.8f, -10f);
+  Vector3 _offset;
+  Vector2 _cameraOffset;
   float smoothingFactor = 6;
+  PlayerInputActions playerInputActions;
+
+  void Start()
+  {
+    playerInputActions = new PlayerInputActions();
+    playerInputActions.Camera.Enable();
+  }
 
   void FixedUpdate()
   {
-    transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothingFactor * Time.fixedDeltaTime);
+    _cameraOffset = playerInputActions.Camera.MoveCamera.ReadValue<Vector2>() * 1.5f;
+    _offset = new Vector3(offset.x + _cameraOffset.x, offset.y + _cameraOffset.y, offset.z);
+    transform.position = Vector3.Lerp(transform.position, target.position + _offset, smoothingFactor * Time.fixedDeltaTime);
   }
 }
