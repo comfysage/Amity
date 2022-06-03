e30f7dd6caa2cb94758d07dda334f9da48ec41a7
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-06-03 14:15:06 +0200

    Attack with OverlapAreaAll

--- Assets/Scripts/Combat/PlayerAttack.cs
+++ Assets/Scripts/Combat/PlayerAttack.cs
@@ -8,8 +8,13 @@ public class PlayerAttack : MonoBehaviour
 {
   public Animator animator;
 
-  public Transform attackPoint;
+  public float attackPointOffset = 1f;
   public float attackRange = 0.5f;
+  public float attackSize = 0.5f;
+
+  private Vector2 _inputVector;
+  private Vector2 _direction;
+  private Vector3 _activePoint;
 
   [SerializeField]
   public float slashTime = 0.12f;
@@ -22,11 +27,22 @@ public class PlayerAttack : MonoBehaviour
   void Awake()
   {
     playerInputActions.Player.Slash.performed += SlashAction;
+    _direction = new Vector2(1, 0);
+  }
+
+  void FixedUpdate()
+  {
+    _inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+    _direction = new Vector2(Mathf.Sign(transform.rotation.y), _inputVector.y);
+    _activePoint = transform.position;
+
+    _activePoint.y += _inputVector.y;
+    _activePoint.x += _inputVector.y != 0 ? _inputVector.x : _direction.x;
   }
 
   void SlashAction(InputAction.CallbackContext context)
   {
-    if (currentState == playerState.Normal && _slashFatigue == 0)
+    if (currentState != playerState.Dashing && _slashFatigue == 0)
     {
       currentState = playerState.Slashing;
       // animator.SetTrigger("Attack"); // Add Attack animation
@@ -35,7 +51,16 @@ public class PlayerAttack : MonoBehaviour
   }
   IEnumerator Slash()
   {
-    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+    bool _vertical = _direction.y != 0;
+
+    float LinearOffset = _vertical ? attackRange * _direction.y : attackRange * _direction.x;
+    float SizeOffset = _vertical ? attackSize : attackSize;
+
+    float _xOffset = _vertical ? SizeOffset / 2 : LinearOffset;
+    float _yOffset = _vertical ? LinearOffset : SizeOffset / 2;
+    Collider2D[] hitEnemies = Physics2D.OverlapAreaAll(
+      new Vector2(_activePoint.x + (_vertical ? _xOffset : 0), _activePoint.y + (_vertical ? 0 : _yOffset)),
+      new Vector2(_activePoint.x + (_vertical ? -_xOffset : _xOffset), _activePoint.y + (_vertical ? _yOffset : -_yOffset)), enemyLayers);
 
     foreach (Collider2D enemy in hitEnemies)
     {
@@ -61,9 +86,20 @@ public class PlayerAttack : MonoBehaviour
 
   void OnDrawGizmosSelected()
   {
-    if (attackPoint == null)
+    if (_activePoint == null)
       return;
 
-    Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    bool _vertical = _direction.y != 0;
+
+    float LinearOffset = _vertical ? attackRange * _direction.y : attackRange * _direction.x;
+    float SizeOffset = _vertical ? attackSize : attackSize;
+
+    float _xOffset = _vertical ? SizeOffset / 2 : LinearOffset;
+    float _yOffset = _vertical ? LinearOffset : SizeOffset / 2;
+
+    Gizmos.DrawLine(
+      new Vector3(_activePoint.x + (_vertical ? _xOffset : 0), _activePoint.y + (_vertical ? 0 : _yOffset), _activePoint.z),
+      new Vector3(_activePoint.x + (_vertical ? -_xOffset : _xOffset), _activePoint.y + (_vertical ? _yOffset : -_yOffset), _activePoint.z)
+      );
   }
 }
